91db0d2dcaa05c88982525d117c30df6a4bb5ff8
Ashish Kumar <kumar.as@husky.neu.edu>
2017-03-07 23:48:57 -0500

    Initial Create-React-App setup

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import Projects from './Components/Projects';
+import uuid from 'uuid';
+import AddProject from './Components/AddProject';
+import './App.css';
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            projects:[]
+        }
+    }
+
+    componentWillMount(){
+        this.setState({projects:[
+            {
+                id:uuid.v4(),
+                title: 'abc',
+                category: 'alpha'
+            },
+            {
+                id:uuid.v4(),
+                title: 'fb',
+                category: 'social'
+
+            },
+            {
+                id:uuid.v4(),
+                title: 'bdsm',
+                category: 'adult'
+            }
+        ]})
+
+    }
+
+    handleAddProject(project){
+       // console.log(project);
+        let projects = this.state.projects;
+        projects.push(project);
+        this.setState({projects:projects});
+
+    }
+
+
+    handleDeleteProject(id){
+        let projects = this.state.projects;
+        let index = projects.findIndex(x =>x.id === id);
+        projects.splice(index,1);
+        this.setState({projects:projects});
+    }
+
+
+  render() {
+    return (
+      <div className="App">
+          <div className="App-header"></div>
+          <div className="container">
+         <AddProject addProject={this.handleAddProject.bind(this)}/>
+          <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
